64d52b2ecd46db99437eb0f3e3525aa323f5ff54
vagrant <vagrant@homestead>
2019-10-24 14:21:03 +0000

    マイグレーションファイルの作成

---
+++ database/migrations/2019_10_24_140010_create_memos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('event'); // 部位
+            $table->binary('data'); // 種目名
+            $table->date('day'); // 日付
+            $table->string('place'); // 場所
+            $table->boolean('isprivate'); // 一応
+            $table->text('memo'); // 備考
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,51 @@
 
+## 機能
+
+- 筋トレ記録を新規投稿できる
+- 筋トレ記録の一覧を表示できる
+- 筋トレ記録の詳細を表示できる
+- 筋トレ記録を削除できる
+
+
 ## Laravel プロジェクトの作成
 
 composer create-project --prefer-dist laravel/laravel KintoreMemo
+
+
+## マイグレーションファイルの作成
+
+php artisan make:migration create_memos_table
+
+json型がワケあって使用できないので、binary型で代用する。
+
+
+## マイグレーションの実行
+
+php artisan migrate
+
+
+| 型 | カラム名 |
+----|---- 
+| bigIncrements | id |
+| string | event |
+| binary | data |
+| date | day |
+| string | place |
+| boolean | isprivate |
+| text | memo |
+| timestamps |  |
+
+
+mysql> show tables;
++-----------------------+
+| Tables_in_kintorememo |
++-----------------------+
+| failed_jobs           |
+| memos                 |
+| migrations            |
+| password_resets       |
+| users                 |
++-----------------------+
+
 
 
