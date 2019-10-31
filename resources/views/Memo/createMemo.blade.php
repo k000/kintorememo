9d090781657a34fd5b220f167c886bba177f91db
vagrant <vagrant@homestead>
2019-10-31 12:12:36 +0000

    jsの修正

--- resources/views/Memo/createMemo.blade.php
+++ resources/views/Memo/createMemo.blade.php
@@ -66,7 +66,42 @@
 </div>
 </div>
 
+
+
+<script>
+const addBtn = document.getElementById('addBtn');
+const delBtn = document.getElementById('delBtn');
+addBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const dataArea = document.getElementById('data-area-outer');
+    const lastChild = dataArea.lastElementChild;
+    const cloneNode = lastChild.cloneNode(true);
+    cloneNode.dataset.count = parseInt(lastChild.dataset.count) + 1;
+    
+    let childs = cloneNode.children;
+   
+    childs[0].children[0].name = "shumoku[" + cloneNode.dataset.count + "]";
+    childs[1].children[0].children[1].name = "weight[" + cloneNode.dataset.count + "]";
+    childs[1].children[1].children[1].name = "rep[" + cloneNode.dataset.count + "]";
+    childs[1].children[2].children[1].name = "set[" + cloneNode.dataset.count + "]";
+
+    dataArea.appendChild(cloneNode);
+});
+delBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const dataArea = document.getElementById('data-area-outer');
+    const lastChild = dataArea.lastElementChild;
+
+    if(lastChild.dataset.count === "0")
+        return;
+    else
+        dataArea.removeChild(lastChild);
+});
+</script>
+
+<!--
 <script src="{{ asset('/js/main.js') }}"></script>
+-->
 
 @endsection
 
--- resources/views/Memo/memo.blade.php
+++ resources/views/Memo/memo.blade.php
@@ -3,18 +3,63 @@
 @section('content')
 
 
+<style>
+    
+    #memo-content-outer{
+        width:90%;
+        max-width:1200px;
+        border:1px solid #000;
+        margin:0 auto;
+    }
 
-    <h1>個別記事</h1>
+    #memo-content-outer .head-area{
+        padding:10px;
+        display:flex;
+        justify-content: space-between;
+    }
+    #memo-content-outer .head-area h1,p{
+        padding:0;
+        margin:0;
+    }
+    
 
-    {{$datas}}
 
-    @for($i=0; $i < count($datas->data->shumoku); $i++)
-        種目{{$datas->data->shumoku[$i]}}
-        重さ{{$datas->data->weight[$i]}}
-        回数{{$datas->data->rep[$i]}}
-        セット{{$datas->data->set[$i]}}
-    @endfor
+</style>
 
+<div id="memo-content-outer">
+    
+    <div class="head-area">
+        <h1>{{$datas->event}}あき高田健志ってすごいよなぁ！？</h1>
+        <p>{{$datas->place}}</p>
+        <small>{{$datas->day}}</small>
+    </div>
+
+   
+
+    <table>
+        <thead>
+            <tr class="table100-head">
+                <th class="column1">種目</th>
+                <th class="column2">重量</th>
+                <th class="column3">レップ数</th>
+                <th class="column4">セット数</th>
+            </tr>
+        </thead>
+        <tbody>    
+            @for($i=0; $i < count($datas->data->shumoku); $i++)
+                <tr>
+                    <td class="column{{$i}}">{{$datas->data->shumoku[$i]}}</td>    
+                    <td class="column{{$i}}">{{$datas->data->weight[$i]}}</td>    
+                    <td class="column{{$i}}">{{$datas->data->rep[$i]}}</td>    
+                    <td class="column{{$i}}">{{$datas->data->set[$i]}}</td>    
+                </tr>
+            @endfor
+        </tbody>
+    </table>
+
+    <p>{{$datas->memo}}</p>
+  
+</div>
     
 
 
